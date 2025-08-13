9a4fbbad4b660da116f34a657e6d9b48e1c1b16b
André Hammarstig <andrehammarstig@gmail.com>
2025-08-13 19:15:23 +0200

    add tests

--- packages/db-seeder-json-file/src/config/config.ts
+++ packages/db-seeder-json-file/src/config/config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   nodeEnv: process.env.NODE_ENV || "development",
   serverHost: process.env.DB_SEEDER_HOST || "localhost",
   serverPort: Number(process.env.DB_SEEDER_PORT) || 3000,
-  filePath: process.env.DB_SEEDER_FILE_PATH || "./db-seeder-data/db.json",
+  filePath: process.env.DB_SEEDER_FILE_PATH || "./data/db.json",
 };
 
 export default config;
---
+++ packages/db-seeder-json-file/src/repositories/__tests__/test.ps1
@@ -0,0 +1,1 @@
+hurl --test ./src/repositories/__tests__/*.hurl
